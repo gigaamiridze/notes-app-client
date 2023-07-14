64b960172dfd3c106e9be6bb9a44c88755be1033
Giga Amiridze <gigaamiridze@gmail.com>
2023-07-14 19:25:22 +0400

    Added http get request to get note by id

--- src/pages/Note.tsx
+++ src/pages/Note.tsx
@@ -1,6 +1,31 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { INote } from '../interfaces';
+import { ApiRoutes } from '../constants';
+
 function Note() {
+  const { id } = useParams();
+  const [note, setNote] = useState<INote | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    getNote();
+  }, [id]);
+
+  const getNote = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.get(`${ApiRoutes.NOTES}/${id}`);
+      setNote(data);
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(true);
+    }
+  }
+
   return (
-    <div>Note</div>
+    <div>{note?.body}</div>
   )
 }
 
