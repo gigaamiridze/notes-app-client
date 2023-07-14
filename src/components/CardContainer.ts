e2a16886a1f7f9b41834c64675545125f2f4f530
Giga Amiridze <gigaamiridze@gmail.com>
2023-07-14 20:58:19 +0400

    Added card container component and its styles

---
+++ src/components/CardContainer.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  max-width: 480px;
+  width: 100%;
+  height: 88vh;
+  background-color: ${({ theme }) => theme.colors.white};
+  box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.05);
+  border-radius: 7px;
+  position: relative;
+`;
+
+export default CardContainer;
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../layouts';
+import { CardContainer } from '../components';
 
 function Root() {
   return (
-    <>
+    <CardContainer>
       <Header />
       <Outlet />
-    </>
+    </CardContainer>
   )
 }
 
